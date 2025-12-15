ac4c1506ffd4a92fd8d094ef707399c3bac4a037
Arian Ajdari <ajdari.arian94@gmail.com>
2025-12-15 10:38:09 +0000

    Logger class has been refactored

--- libraries/utils/include/aember-libs/utils/logging/logging.h
+++ libraries/utils/include/aember-libs/utils/logging/logging.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/logger.h>
 #include <spdlog/spdlog.h>
 
 #include <memory>
@@ -8,19 +8,57 @@
 
 namespace aember::utils {
 
+/**
+ * Initialize early logging (stdout only).
+ * Call once during early init.
+ */
+void init_early_logging();
+
+/**
+ * Enable file logging after pivot_root.
+ * Safe to call once real root is mounted.
+ */
+void enable_file_logging(const std::string& log_file_path);
+
+/**
+ * Lightweight per-component logger.
+ */
 class Logger {
  public:
-  Logger(const std::string& class_name);
+  explicit Logger(const std::string& name);
 
-  // Logging functions
-  void info(const std::string& message, const std::string& func = "");
-  void warn(const std::string& message, const std::string& func = "");
-  void error(const std::string& message, const std::string& func = "");
-  void debug(const std::string& message, const std::string& func = "");
+  template <typename... Args>
+  void trace(fmt::format_string<Args...> fmt, Args&&... args) {
+    logger_->trace(fmt, std::forward<Args>(args)...);
+  }
+
+  template <typename... Args>
+  void debug(fmt::format_string<Args...> fmt, Args&&... args) {
+    logger_->debug(fmt, std::forward<Args>(args)...);
+  }
+
+  template <typename... Args>
+  void info(fmt::format_string<Args...> fmt, Args&&... args) {
+    logger_->info(fmt, std::forward<Args>(args)...);
+  }
+
+  template <typename... Args>
+  void warn(fmt::format_string<Args...> fmt, Args&&... args) {
+    logger_->warn(fmt, std::forward<Args>(args)...);
+  }
+
+  template <typename... Args>
+  void error(fmt::format_string<Args...> fmt, Args&&... args) {
+    logger_->error(fmt, std::forward<Args>(args)...);
+  }
+
+  template <typename... Args>
+  void critical(fmt::format_string<Args...> fmt, Args&&... args) {
+    logger_->critical(fmt, std::forward<Args>(args)...);
+  }
 
  private:
   std::shared_ptr<spdlog::logger> logger_;
-  std::string class_name_;
 };
 
 }  // namespace aember::utils
