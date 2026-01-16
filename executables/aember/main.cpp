19d546549dc0b9f0561962756c31573792c5d598
Arian Ajdari <arian@cybee.ai>
2026-01-16 13:10:06 +0000

    Initial structure

--- executables/aember/main.cpp
+++ executables/aember/main.cpp
@@ -1,6 +1,21 @@
 #include <aember-libs/aember-init/aember-init.h>
 
+#include <CLI/CLI.hpp>
+
 int main(int argc, char** argv) {
+  CLI::App app{"Aember"};
+
+  bool initramfs = false;
+  bool root = false;
+
+  app.add_flag("--initramfs", initramfs, "Start in initramfs mode");
+  app.add_flag("--root", root, "Start in root mode");
+
+  // Enforce exactly one mode
+  app.require_option(1);
+
+  CLI11_PARSE(app, argc, argv);
+
   aember::aember_init::AemberInit aember_init{argc, argv};
 
   aember_init.Start();
